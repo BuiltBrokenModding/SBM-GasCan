fc6dd9afb4149e61f49960e0831978e6ff8c8f79
Robert Seifert <rseifert.phone@gmail.com>
2017-03-07 03:45:18 -0500

    Added dependency info

--- src/main/java/com/builtbroken/gascan/GasCanMod.java
+++ src/main/java/com/builtbroken/gascan/GasCanMod.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 3/7/2017.
  */
-@cpw.mods.fml.common.Mod(modid = "sbmgascan", name = "Gas Can", version = GasCanMod.VERSION)
+@cpw.mods.fml.common.Mod(modid = "sbmgascan", name = "Gas Can", version = GasCanMod.VERSION, dependencies = "after:vefluids")
 public class GasCanMod
 {
     public static final String MAJOR_VERSION = "@MAJOR@";
